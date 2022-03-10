d34efc9e9f5255d6e5526c42617b915fcee6854d
Christian Agustin <networker@christianagustin.com>
2022-03-10 11:06:40 -0600

    se crear el cóigo para publish

---
+++ publish/Publish.groovy
@@ -0,0 +1,23 @@
+@Grab('org.apache.poi:poi:3.8')
+@Grab('org.apache.poi:poi-ooxml:3.8')
+import org.apache.poi.ss.usermodel.*
+import org.apache.poi.hssf.usermodel.*
+import org.apache.poi.xssf.usermodel.*
+import org.apache.poi.ss.util.*
+import org.apache.poi.ss.usermodel.*
+import java.io.*
+
+class Publish {
+    static void main(String[] args){
+        InputStream inp = new FileInputStream(args[0])
+        Workbook wb = WorkbookFactory.create(inp)
+        Sheet sheet = wb.getSheetAt(0)
+        Iterator<Row> rowIt = sheet.rowIterator()
+
+        while(rowIt.hasNext()) {
+            Row row = rowIt.next()
+            Cell cell = row.getCell(0)
+            println cell.getRichStringCellValue().getString()
+        }
+    }
+}
