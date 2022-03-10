ebf7b1324218ddcc93a13d4e5a4c05d600fcf981
Christian Agustin <networker@christianagustin.com>
2022-03-09 23:45:32 -0600

    se actualiza script

--- validarypublicar/script.groovy
+++ validarypublicar/script.groovy
@@ -1,4 +1,5 @@
 def validate(project) {
+    echo "valideate${project}"
     def response = httpRequest url:"http://100.126.0.13:7004/ecm/ecm/CatalogManagement/v2/project/${project}/validate", 
         httpMode: 'POST', customHeaders: [[name: 'OnBehalfOf', value: 'upadmin']]
         
